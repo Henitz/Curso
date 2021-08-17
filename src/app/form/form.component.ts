1ac1bacd088cda5d4534e1d00f63fbc873fb5134
Henitz <henitz@gmail.com>
2021-08-17 14:47:32 -0300

    atualizar

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { faSave } from '@fortawesome/free-solid-svg-icons';
 import { Clientes } from '../clientes';
 import { ClientesService } from '../clientes.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+
+import { faSave, faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
+
+//parte 2
+import { Params, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form',
@@ -15,15 +20,38 @@ export class FormComponent implements OnInit {
   cliente: Clientes;
 
   faSave=faSave;
+  faArrowCircleLeft=faArrowCircleLeft;
+
+    //parte 2
+    id!: number;
 
   constructor(
     private service: ClientesService,
-    private router: Router
+    private router: Router,
+
+    //parte 2
+    private activatedRoute: ActivatedRoute
+
+
   ) {
     this.cliente = new Clientes();
   }
 
   ngOnInit(): void {
+
+    let params : Observable<Params> = this.activatedRoute.params
+    params.subscribe( urlParams => {
+      this.id = urlParams['id']
+      if(this.id){
+        this.service
+              .getOne(this.id)
+              .subscribe(
+                response => this.cliente = response ,
+                errorResponse => this.cliente = new Clientes()
+              )
+      }
+    })
+
   }
 
   save() {
