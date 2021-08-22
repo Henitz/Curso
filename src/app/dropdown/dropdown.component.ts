cb245975584c98bedf370067fe4227ab49d17bc7
Henitz <henitz@gmail.com>
2021-08-22 15:30:54 -0300

    Cidades

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from '../country';
 import { State } from '../state';
+import { City } from '../city';
 import { SelectService } from '../select.service';
 
 @Component({
@@ -11,8 +12,10 @@ import { SelectService } from '../select.service';
 export class DropdownComponent implements OnInit {
 
   selectedCountry: Country = new Country('Brasil');
+  selectedState: State = new State('São Paulo', 'Brasil');
   countries!: Country[];
   states!: State[];
+  cities!: City[];
 
   constructor(private selectService: SelectService) {}
 
@@ -22,5 +25,14 @@ export class DropdownComponent implements OnInit {
 
   onSelect(countryName: string) {
     this.states = this.selectService.getStates().filter((s) => s.CountryName == countryName)
+  }
+
+  onSelectState(stateName: string) {
+    console.log("Estado selecionado: " + stateName);
+    this.cities = this.selectService.getCities().filter((c) => {
+      c.StateName == stateName;
+      console.log("Lista de estado na posição: " + c.StateName)
+    }
+    )
   }
 }
--- src/app/select.service.ts
+++ src/app/select.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Country } from "./country";
 import { State } from "./state";
+import { City } from "./city";
 
 @Injectable()
 export class SelectService {
   countries: Country[] = [];
   states: State[] = [];
+  cities: City[] = [];
 
   urlCountries :string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/Countries.json";
   urlStates :string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/States.json";
+  urlCities : string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/Cities.json"
 
   constructor(private http:HttpClient) { }
 
@@ -19,7 +22,13 @@ export class SelectService {
   }
 
   allStates(): Observable<any>{
+    console.log("######SERVICE ESTADOS#########" + this.http.get(this.urlStates));
     return this.http.get(this.urlStates);
+  }
+
+  allCities(): Observable<any>{
+    console.log("######SERVICE CIDADES#########" + this.http.get(this.urlCities));
+    return this.http.get(this.urlCities);
   }
 
   getCountries(){
@@ -29,6 +38,13 @@ export class SelectService {
 
   getStates(){
     this.allStates().subscribe(c => {this.states=c.States;})
+    console.log("Tenho os estados? " + this.states)
     return this.states;
+  }
+
+  getCities(){
+    this.allCities().subscribe(c => {this.cities=c.Cities;})
+    console.log("Tenho as cidades? " + this.cities)
+    return this.cities;
   }
 }
