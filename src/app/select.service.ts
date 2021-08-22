223a8604f8de35be6d1b688920aeb0b4dc70c24a
Henitz <henitz@gmail.com>
2021-08-22 17:19:14 -0300

    Campo cidade

--- src/app/select.service.ts
+++ src/app/select.service.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
 import { Country } from "./country";
 import { State } from "./state";
 import { City } from "./city";
@@ -11,30 +9,7 @@ export class SelectService {
   states: State[] = [];
   cities: City[] = [];
 
-  urlCountries :string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/Countries.json";
-  urlStates :string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/States.json";
-  urlCities : string = "https://raw.githubusercontent.com/Henitz/Curso/develop/src/assets/Cities.json"
-
-  constructor(private http:HttpClient) { }
-
-  allCountries(): Observable<any>{
-    return this.http.get(this.urlCountries);
-  }
-
-  allStates(): Observable<any>{
-    console.log("######SERVICE ESTADOS#########" + this.http.get(this.urlStates));
-    return this.http.get(this.urlStates);
-  }
-
-  allCities(): Observable<any>{
-    console.log("######SERVICE CIDADES#########" + this.http.get(this.urlCities));
-    return this.http.get(this.urlCities);
-  }
-
-  _getCountries(){
-    this.allCountries().subscribe(c => {this.countries=c.Countries;})
-    return this.countries;
-  }
+  constructor() { }
 
   getCountries() {
     return [
@@ -42,7 +17,7 @@ export class SelectService {
      new Country('Brasil'),
     ];
   }
-  
+
   getStates() {
    return [
      new State('United States', 'Arizona' ),
@@ -55,23 +30,10 @@ export class SelectService {
     ];
   }
 
-    
   getCities() {
     return [
       new City('United States', 'New York', 'New York'),
       new City('Brasil', 'São Paulo', 'São Paulo' ),
      ];
    }
-
-  _getStates(){
-    this.allStates().subscribe(c => {this.states=c.States;})
-    console.log("Tenho os estados? " + this.states)
-    return this.states;
-  }
-
-  _getCities(){
-    this.allCities().subscribe(c => {this.cities=c.Cities;})
-    console.log("Tenho as cidades? " + this.cities)
-    return this.cities;
-  }
 }
