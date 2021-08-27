de592e1856eb4927f1521c595eab4837506a0b3e
andretecnologia <andre@andretecnologia.com.br>
2021-08-27 16:29:14 -0300

    grafico incluido

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,25 +1,55 @@
 import { Component } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
+import { Clientes } from '../clientes';
+import { ClientesService } from '../clientes.service';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-
 export class BarChartComponent {
+
+  clientes: Clientes[] = [];
+ 
+  constructor(private service: ClientesService) { 
+  }
+
+  ngOnInit(): void {
+      this.service.getAll().subscribe(   
+      data =>{
+        this.barChartData[0].data = data.map(c=> parseInt((c.quantidadeFuncionarios).toString())) 
+        this.barChartLabels = data.map(c => c.nome)
+      })
+  }
 
   barChartOptions: ChartOptions = {
     responsive: true,
+    scales: { yAxes: [{ ticks: { beginAtZero: true } }] }
   };
-  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  barChartColors: Color[] = [
+  {
+    borderColor: [
+      'rgba(255,0,0,0.5)',
+      'rgba(54, 75, 181, 0.5)',
+      'rgba(114, 155, 59, 0.5)',
+      'rgba(102, 59, 155, 0.5)'
+    ],
+    backgroundColor: [
+      'rgba(255,0,0,0.3)',
+      'rgba(54, 75, 181, 0.3)',
+      'rgba(114, 155, 59, 0.3)',
+      'rgba(102, 59, 155, 0.3)'
+    ]
+  }];
+
+  barChartLabels: Label[] = [];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
 
   barChartData: ChartDataSets[] = [
-    { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits' }
+    { data: [], label: 'Quantidade de clientes' }
   ];
-
 }
